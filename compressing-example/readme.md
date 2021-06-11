eabcfef2d56d5da560747e30732f3cbef7d23e8e
Владислав Наумов <vnaumov@mirantis.com>
2021-06-11 11:37:51 +0200

    [arrays] decode string

---
+++ compressing-example/readme.md
@@ -0,0 +1,2 @@
+#Task
+Unpack 4A10BD3C -> AAAABBBBBBBBBBDCCC, yield error if >50 chars
---
+++ compressing-example/unpack.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"unicode"
+)
+
+func decodeStr(s string) (string, error) {
+	res := ""
+	counter := "" // base for our multiplicator
+
+	for i := 0; i < len(s); i++ {
+		symbol := rune(s[i])
+
+		if unicode.IsLetter(symbol) {
+			slice := []rune{symbol}
+
+			if counter != "" {
+				multiplicator, err := strconv.Atoi(counter)
+				if err != nil {
+					return res, err
+				}
+
+				//  c = 1 coz we already have 1 symbol
+				for c := 1; c < multiplicator; c++ { // <<<<<<< bad time/space complexity!
+					slice = append(slice, symbol)
+				}
+			}
+			res += string(slice)
+			// reset counter
+			counter = ""
+		} else {
+			counter += string(symbol)
+		}
+
+		// quota check
+		if len(res) > 50 {
+			return res, errors.New("Fail: 50 symbols quota reached")
+		}
+	}
+
+	// if last symbol is a number
+	if counter != "" {
+		res += counter
+	}
+
+	return res, nil
+}
+
+func main() {
+	testStr := "4A10BD3C"
+	testFailStr := "10A10B10C20D6E"
+
+	encOne, err := decodeStr(testStr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Str %v encoded %v\n", testStr, encOne)
+
+	encTwo, err := decodeStr(testFailStr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Str %v encoded %v\n", testFailStr, encTwo)
+}
